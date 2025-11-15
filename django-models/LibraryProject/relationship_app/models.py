ef6a688aedc71b0bb3e9e10324fef1fefa0c9413
Peter Kimani <p.kimani232@gmail.com>
2025-11-16 00:39:00 +0300

    Deep dive django views, models, views

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+# Existing Models
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name= 'libraries')
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name= 'librarian')
+
+    def __str__(self):
+        return f"{self.name} ({self.library.name})" 
