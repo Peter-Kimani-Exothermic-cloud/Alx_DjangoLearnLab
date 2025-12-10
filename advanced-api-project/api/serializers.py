24b385990b419c14bd3fc0c5f828a9a7de50fe1f
Peter Kimani <p.kimani232@gmail.com>
2025-12-11 00:00:01 +0300

    Debbugging api.serializers

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -16,7 +16,7 @@ class BookSerializer(serializers.ModelSerializer):
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    books = BookSerializer(many=True, read_only = True)
+    books = BookSerializer["(many=True, read_only=True)"]
     class Meta:
         model = Author
         fields = ['id', 'name', 'books']
